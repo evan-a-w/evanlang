649dd624b22ad3c6d6abe57aa5349e2bba422cde
Evan Williams <evanawilliams@live.com>
2022-06-04 20:22:54 +1000

    😊

--- frontend/lang.el
+++ frontend/lang.el
@@ -6,8 +6,6 @@
 (deftype (<type_params> <name>) (where (<type_param> is [<trait>, ...]) ...)?
   ( <variant> of <type>?, ... ))
 
-(fn (<params>) (<expr>))
+(fn (<params>) <expr>)
 
 type = a b identifier | type list sep by "->" | [[ <trait name>, ... ]]
-
-egs in parser_tests.ml
