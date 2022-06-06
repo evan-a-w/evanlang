2b2a0045089356b662ca2bb009db7c7567d5a175
Evan Williams <evanawilliams@live.com>
2022-06-06 21:16:10 +1000

    😊

--- middleend/parse_state.ml
+++ middleend/parse_state.ml
@@ -6,28 +6,32 @@ open Types
 type parse_state = {
   t_state : Types.type_state;
   v_env : var_env;
-  t_map : Frontend.Types.type_lit Types.StringMap.t;
+  t_map : stored_type Types.StringMap.t;
 } and var_env = {
   var : Types.typ ref;
   prev : var_env option;
-} and type_lit =
-  [ `Sum of (string * Types.typ) list
-  | `Prod of (string * Types.typ) list ]
+} and stored_type = {
+  params: (string * (string list)) list;
+  type_lit: Frontend.Types.type_lit;
+}
 
 let add_type ps (t_expr, t_spec, t_lit) = let open Option.Let_syntax in
   let%bind (param_list, type_name) = match t_expr with
     | `Single x -> Some x
     | _ -> None in
-  let generic_list =
+  let traits_list =
     let get_trait_list s =
       Option.value ~default:[]
                    (List.Assoc.find ~equal:String.equal t_spec s) in
-    let f s = (s, Types.Generic_ (Types.TraitSet.of_list (get_trait_list s))) in
+    let f s = (s, get_trait_list s) in
     List.map ~f:f param_list in
+  let map_generic_type = List.map ~f:(fun (s, trait_list) ->
+                                  (s, Types.Generic_ (Types.TraitSet.of_list trait_list))) in
+  let generic_list = map_generic_type traits_list in
   let type_params = Types.StringMap.of_seq (Stdlib.List.to_seq generic_list) in
   let new_type = Types.Concrete_ { type_name; type_params } in
   Some { ps with t_state = Types.add_type ps.t_state (type_name, new_type);
-                 t_map = Types.StringMap.add type_name t_lit ps.t_map;
+                 t_map = Types.StringMap.add type_name { type_lit = t_lit; params = traits_list } ps.t_map;
        }
 
 let folding_map_option :
--- middleend/process.ml
+++ middleend/process.ml
@@ -3,32 +3,51 @@ open Type_sys
 open Type_sys.Defaults
 open Core
 
+let type_from_t_expr p_state = function
+  | `Single (params, name) ->
+      let%bind { t_params; t_lit } = Types.StringMap.find_opt name p_state.t_map in
+      
+  | `Multi l ->
+
+let rec define_types p_state = function
+  | `DefType (t_expr, t_spec, t_lit) ->
+      let type_added = add_type p_state (t_expr, t_spec, t_lit) in
+      type_added, None
+  | x -> p_state, Some x
+
 let rec generate_initial_types p_state = let open Option.Let_syntax in
   let fold_state seq state = Parse_state.folding_map_option seq ~f:generate_initial_types ~init:state in
   let of_list = Base.Sequence.of_list in
   let to_list = Base.Sequence.to_list in
   function
-  | `Unit -> p_state, (`Unit, Types.Concrete_ unit_type) |> Option.some
-  | `Bool b -> p_state, (`Bool b, Types.Concrete_ bool_type) |> Option.some
-  | `FloatLit f -> p_state, (`Float f, Types.Concrete_ float_type) |> Option.some
-  | `IntLit i -> p_state, (`IntLit i, Types.Concrete_ int_type) |> Option.some
-  | `StringLit s -> p_state, (`StringLit s, Types.Concrete_ string_type) |> Option.some
-  | `SymbolLit s -> p_state, (`SymbolLit s, Types.Concrete_ symbol_type) |> Option.some
-  | `Identifier i -> p_state, (`Identifier i, any_type) |> Option.some
+  | `Unit -> (p_state, (`Unit, Types.Concrete_ unit_type)) |> Option.some
+  | `Bool b -> (p_state, (`Bool b, Types.Concrete_ bool_type)) |> Option.some
+  | `FloatLit f -> (p_state, (`FloatLit f, Types.Concrete_ float_type)) |> Option.some
+  | `IntLit i -> (p_state, (`IntLit i, Types.Concrete_ int_type)) |> Option.some
+  | `StringLit s -> (p_state, (`StringLit s, Types.Concrete_ string_type)) |> Option.some
+  | `SymbolLit s -> (p_state, (`SymbolLit s, Types.Concrete_ symbol_type)) |> Option.some
+  | `Identifier i -> (p_state, (`Identifier i, any_type)) |> Option.some
   | `ListLit l ->
       let%bind new_p_state, mapped_seq = fold_state (of_list l) p_state in
       let mapped_list = to_list mapped_seq in
       Some (new_p_state, (`ListLit mapped_list, Types.Concrete_ list_type))
-  | `ArrayLit a -> Some p_state, (`ArrayLit (Array.map ~f:gwithp a), Types.Concrete_ array_type)
+  | `ArrayLit a ->
+      let seq = Stdlib.Array.to_seq a |> Base.Sequence.of_seq in
+      let%bind new_p_state, mapped_seq = fold_state seq p_state in
+      let mapped_arr = Base.Sequence.to_seq mapped_seq |> Stdlib.Array.of_seq in
+      Some (new_p_state, (`ArrayLit mapped_arr, Types.Concrete_ array_type))
   | `Call (f, l) ->
-    let new_opt_p_state, tf = generate_initial_types p_state f in
-    let%bind new_p_state = new_opt_p_state in
+    let%bind new_p_state, tf = generate_initial_types p_state f in
     let _, t = tf in
     let open Base.Sequence in
-    let newer_opt_p_state, mapped_seq = fold_state (of_list l) new_p_state in
-    let%bind newer_p_state = newer_opt_p_state in
-    (new_p_state, (`Call (tf, List.map ~f:gwithp l), t))
-  | `DefType (t_expr, t_spec, t_lit) -> Some p_state, (`Unit, Types.Concrete_ unit_type)
-  | `Typed (t_expr, e) -> Some p_state, (`Unit, Types.Concrete_ unit_type)
-  | `Let (name, e) -> Some p_state, (`Unit, Types.Concrete_ unit_type)
-  | `Function (args, e) -> Some p_state, (`Unit, Types.Concrete_ unit_type)
+    Option.bind (fold_state (of_list l) new_p_state) ~f:(fun (newer_p_state, mapped_seq) ->
+    let mapped_list = to_list mapped_seq in
+    Some (newer_p_state, (`Call (tf, mapped_list), t)))
+  | `DefType (t_expr, t_spec, t_lit) ->
+      let type_added = add_type p_state (t_expr, t_spec, t_lit) in
+      Some (type_added, (`Unit, Types.Concrete_ unit_type))
+  | `Typed (t_expr, e) ->
+      let%bind new_p_state, (te, _) = generate_initial_types p_state e in
+      Some (new_p_state, (te, type_from_t_expr t_expr))
+  | `Let (name, e) -> Some (p_state, (`Unit, Types.Concrete_ unit_type))
+  | `Function (args, e) -> Some (p_state, (`Unit, Types.Concrete_ unit_type))
